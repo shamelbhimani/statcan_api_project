e194018db859928ec724ead5eef39a6a47450690
Shamel Bhimani <shamelbhimani@outlook.com>
2025-09-22 16:14:35 -0400

    Updated to handle table definitions

--- src/definitions_fetcher.py
+++ src/definitions_fetcher.py
@@ -5,27 +5,37 @@ def get_definitions(config_path: str = '../config/config.ini') -> dict[Any,
                                                                         Any]:
     config = configparser.ConfigParser()
     config.read(config_path)
-    path = config.get('paths', 'vectors_definitions_file')
+    path1 = config.get('paths', 'vectors_definitions_file')
+    path2 = config.get('paths', 'table_definitions_file')
+    list_of_paths = [path1, path2]
     definitions = {}
 
-    try:
-        with open(path, 'r') as vectors_definitions_file:
-            for line in vectors_definitions_file:
-                line = line.strip()
-                if not line:
-                    continue
+    for path in list_of_paths:
+        try:
+            with open(path, 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    if not line:
+                        continue
 
-                parts = line.split(',', 1)
-                if len(parts) == 2:
-                    key_str = parts[0].replace('v', '').strip()
-                    value_str = parts[1].strip().strip('"')
+                    parts = line.split(',', 1)
+                    if len(parts) == 2:
+                        if parts[0].startswith('v'):
+                            key_str = parts[0].replace('v',
+                                                       '').strip()
+                            value_str = parts[1].strip().strip('"')
+                        else:
+                            key_str = parts[0].replace('-',
+                                                       '').strip()
+                            value_str = parts[1].strip().strip('"')
 
-                    try:
-                        definitions[int(key_str)] = value_str
-                    except ValueError:
-                        print(f"Skipping malformed line (invalid key): "
-                              f"{line}")
-    except FileNotFoundError:
-        print(f"Error: file {path} not found")
+                        try:
+                            definitions[int(key_str)] = value_str
+                        except ValueError:
+                            print(f"Skipping malformed line (invalid key): "
+                                  f"{line}")
 
+
+        except FileNotFoundError:
+            print(f"Error: file {path} not found")
     return definitions
