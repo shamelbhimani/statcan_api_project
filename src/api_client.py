d62b4d54b6fe2e55650b121fb5e3b7ee43d62bdf
Shamel Bhimani <shamelbhimani@outlook.com>
2025-09-25 11:43:30 -0400

    Changed output structure to ignore list. Now only nests in dictionaries

--- src/api_client.py
+++ src/api_client.py
@@ -24,7 +24,7 @@ class APIClient:
         except FileNotFoundError:
             return []
 
-    def _fetch_data(self, list_of_vectors: list[str], period: int = 1):
+    def _fetch_data(self, list_of_vectors: list[str], period: int = 1) -> None:
         if not list_of_vectors:
             return None
 
@@ -41,9 +41,9 @@ class APIClient:
             print(e)
             return None
 
-    def _extract_data(self) -> dict[Any, Any]:
+    def _extract_data(self) -> None:
         if not self.api_response:
-            return {}
+            return None
 
         for item in self.api_response:
             if (isinstance(item, dict) and item.get('status') == 'SUCCESS' and
@@ -52,24 +52,24 @@ class APIClient:
                 for subitem in item['object']['vectorDataPoint']:
                     sub_data_dict.update({str(subitem.get('refPerRaw')):
                         subitem.get(
-                        'value')})
+                            'value')})
 
                 data_dict = {
                     item['object']['vectorId']: self._sort_dictionary(
-                        sub_data_dict,False)
+                        sub_data_dict, False)
                 }
 
                 if (item['object'].get('productId') in
                         self.extracted_data.keys()):
-                    self.extracted_data[item['object'].get('productId')].append(
+                    self.extracted_data[item['object'].get('productId')].update(
                         data_dict)
                 else:
                     self.extracted_data.update({item['object'].get('productId'):
-                                           [data_dict]})
+                                                    data_dict})
             else:
                 pass
 
-        return self.extracted_data
+        return None
 
     @staticmethod
     def _sort_dictionary(dictionary: dict[str, Any],
@@ -81,7 +81,8 @@ class APIClient:
                                reverse=True))
         return None
 
-    def run(self, period: int = 1) -> dict[str, Any] | None:
+    def run(self, period: int = 1) -> None:
         list_of_vectors = self._get_vectors()
         self._fetch_data(list_of_vectors, period=period)
-        return self._extract_data()
+        self._extract_data()
+        return None
