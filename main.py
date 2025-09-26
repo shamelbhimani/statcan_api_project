8b25b01001f11fb57ce7743b5b3f4eea32fcbb24
Shamel Bhimani <shamelbhimani@outlook.com>
2025-09-26 16:30:34 -0400

    Updated main process and added exception handling and logging.

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
+import logging
+import sys
 from src.api_client import APIClient
 from src.definitions_fetcher import get_definitions
 from src.database_manager import run_process
 
 
 def main():
-    api_client = APIClient('config/config.ini')
-    period = int(input("Enter the period of the period you wish to extract "
-                       "data from: "))
-    api_client.run(period)
-    data = api_client.extracted_data
-    definitions = get_definitions('config/config.ini')
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %("
+               "message)s",
+        handlers=[
+            logging.FileHandler("app.log", mode="w"),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+    logging.info("Application started...")
+    try:
+        api_client = APIClient('config/config.ini')
+        try:
+            period = int(input("Enter the period of the period you wish to extract "
+                               "data from: "))
+        except ValueError:
+            logging.error(f"Invalid input: {period}. Please enter an integer.")
+            return
 
-    run_process(data, definitions, 'config/secrets.ini')
+        api_client.run(period)
+        data = api_client.extracted_data
+        definitions = get_definitions('config/config.ini')
+
+        if not data:
+            logging.critical(f"No data found for period {period}. Aborting "
+                             f"process.")
+            return
+        if not definitions:
+            logging.warning(f'Definitions file might be empty or missing...')
+
+        run_process(data, definitions, 'config/secrets.ini')
+    except Exception as e:
+        logging.critical(f'An unhandled exception occurred: {e}')
+    finally:
+        logging.info("Application ended...")
 
 if __name__ == '__main__':
     main()
