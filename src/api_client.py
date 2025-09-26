22e897c498aee7632a4017b457bb502554bd7cd8
Shamel Bhimani <shamelbhimani@outlook.com>
2025-09-26 15:27:12 -0400

    Added logging and exception handling. Changed a for loop to list comprehension.

--- src/api_client.py
+++ src/api_client.py
@@ -1,58 +1,75 @@
 import configparser
 import requests
 import json
+import logging
 from typing import Any
 
 class APIClient:
     def __init__(self, config_path: str = '../config/config.ini') -> None:
         self.config_path = config_path
         self.config = configparser.ConfigParser()
-        self.config.read(self.config_path)
         self.api_response = None
         self.url = f'https://www150.statcan.gc.ca/t1/wds/rest/getDataFromVectorsAndLatestNPeriods'
         self.extracted_data = {}
+        try:
+            self.config.read(self.config_path)
+            logging.info(f'APIClient initialized for url: {self.url} and '
+                         f'config at path: {self.config_path}')
+        except configparser.Error:
+            logging.error(f'Config file {self.config_path} could not be read.')
 
     def _get_vectors(self) -> list[str]:
-        path = self.config.get('paths', 'vectors_file')
         try:
-            with open(path, 'r') as vector_file:
-                list_of_vectors = []
-                for line in vector_file:
-                    list_of_vectors.append(line.replace('v',
-                                                        '').strip())
+            vector_path = self.config.get('path', 'vectors_file')
+            logging.info(f'Reading vectors from {vector_path}')
+            with open(vector_path, 'r') as vector_file:
+                list_of_vectors = [line.replace('v', '').strip() for line in
+                                   vector_file]
+                logging.info(f"Found {len(list_of_vectors)} vectors.")
                 return list_of_vectors
         except FileNotFoundError:
+            logging.error(f"Could not find file at {self.config_path}.")
+            return []
+        except (configparser.NoSectionError, configparser.NoOptionError) as e:
+            logging.error(f'Configuration error in {self.config_path}: {e}')
             return []
 
     def _fetch_data(self, list_of_vectors: list[str], period: int = 1) -> None:
         if not list_of_vectors:
+            logging.warning('Vector list is empty. Skipping API call...')
             return None
 
         payload = [{'vectorId': v, 'latestN': period} for v in list_of_vectors]
         headers = {'Content-Type': 'application/json'}
 
+        logging.info(f'Fetching data for {len(list_of_vectors)} from'
+                     f'for the latest {period} months from {self.url}...')
         try:
             response = requests.post(self.url, headers=headers, data=json.dumps(
                 payload))
             response.raise_for_status()
             self.api_response = response.json()
+            logging.info(f'API response fetched successfully.')
             return self.api_response
         except requests.exceptions.RequestException as e:
-            print(e)
+            logging.error(f'API request failed: {e}')
             return None
 
     def _extract_data(self) -> None:
         if not self.api_response:
+            logging.warning('No response from API. Skipping API call...')
             return None
 
+        logging.info(f'Extracting data from API response...')
+        extracted_count = 0
+        skipped_count = 0
         for item in self.api_response:
             if (isinstance(item, dict) and item.get('status') == 'SUCCESS' and
                     'object' in item):
                 sub_data_dict = {}
                 for subitem in item['object']['vectorDataPoint']:
                     sub_data_dict.update({str(subitem.get('refPerRaw')):
-                        subitem.get(
-                            'value')})
+                                              subitem.get('value')})
 
                 data_dict = {
                     item['object']['vectorId']: self._sort_dictionary(
@@ -66,9 +83,17 @@ class APIClient:
                 else:
                     self.extracted_data.update({item['object'].get('productId'):
                                                     data_dict})
+                extracted_count += 1
+                logging.info(f'Added {item['object']['vectorId']} to'
+                             f' {item["object"].get("productId")}.')
             else:
-                pass
-
+                skipped_count += 1
+                logging.warning(f"Skipping an item in response "
+                                f"Status: {item.get('status')}, "
+                                f"Object: {item.get('object')}")
+        logging.info(f'Data extracted from API response. Data extracted for '
+                     f'{extracted_count} items. Items skipped:'
+                     f' {skipped_count}.')
         return None
 
     @staticmethod
@@ -82,7 +107,9 @@ class APIClient:
         return None
 
     def run(self, period: int = 1) -> None:
+        logging.info('Starting API communication process...')
         list_of_vectors = self._get_vectors()
         self._fetch_data(list_of_vectors, period=period)
         self._extract_data()
+        logging.info('API call completed.')
         return None
