401ed2043131f2f58c1c282d2910af2f95b94745
Shamel Bhimani <shamelbhimani@outlook.com>
2025-10-02 14:49:00 -0400

    Reformatted.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from src.api_client import APIClient
 from src.definitions_fetcher import get_definitions
 from src.database_manager import run_process
 
+
 def main():
     """
     The main function of the program. Extracts data from the Statistics
@@ -17,40 +18,40 @@ def main():
     """
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %("
-               "message)s",
+        format="%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s",
         handlers=[
             logging.FileHandler("../app.log", mode="w"),
-            logging.StreamHandler(sys.stdout)
-        ]
+            logging.StreamHandler(sys.stdout),
+        ],
     )
     logging.info("Application started...")
     try:
-        api_client = APIClient('../config/config.ini')
+        api_client = APIClient("../config/config.ini")
         period = None
         try:
-            period = int(input("Enter the period of the period you wish to extract "
-                               "data from: "))
+            period = int(
+                input("Enter the period of the period you wish to extract data from: ")
+            )
         except ValueError:
             logging.error(f"Invalid input: {period}. Please enter an integer.")
             return
 
         api_client.run(period)
         data = api_client.extracted_data
-        definitions = get_definitions('../config/config.ini')
+        definitions = get_definitions("../config/config.ini")
 
         if not data:
-            logging.critical(f"No data found for period {period}. Aborting "
-                             f"process.")
+            logging.critical(f"No data found for period {period}. Aborting process.")
             return
         if not definitions:
-            logging.warning('Definitions file might be empty or missing...')
+            logging.warning("Definitions file might be empty or missing...")
 
-        run_process(data, definitions, '../config/secrets.ini')
+        run_process(data, definitions, "../config/secrets.ini")
     except Exception as e:
-        logging.critical(f'An unhandled exception occurred: {e}')
+        logging.critical(f"An unhandled exception occurred: {e}")
     finally:
         logging.info("Application ended...")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
