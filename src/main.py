c1cff50c4932c5ce85738b8f378c765fafd5d8b9
Shamel Bhimani <shamelbhimani@outlook.com>
2025-10-02 15:10:34 -0400

    Using Pathlib to resolve paths

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from pathlib import Path
 from src.api_client import APIClient
 from src.definitions_fetcher import get_definitions
 from src.database_manager import run_process
@@ -16,17 +17,22 @@ def main():
     :except Exception: Raises an exception if an unknown error occurs in the
     process.
     """
+    project_root = Path(__file__).resolve().parent.parent
+    config_path = project_root / "config" / "config.ini"
+    secrets_path = project_root / "config" / "secrets.ini"
+    log_path = project_root / "app.log"
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s",
         handlers=[
-            logging.FileHandler("../app.log", mode="w"),
+            logging.FileHandler(log_path, mode="w"),
             logging.StreamHandler(sys.stdout),
         ],
     )
     logging.info("Application started...")
     try:
-        api_client = APIClient("../config/config.ini")
+        api_client = APIClient(config_path)
         period = None
         try:
             period = int(
@@ -38,7 +44,7 @@ def main():
 
         api_client.run(period)
         data = api_client.extracted_data
-        definitions = get_definitions("../config/config.ini")
+        definitions = get_definitions(config_path)
 
         if not data:
             logging.critical(f"No data found for period {period}. Aborting process.")
@@ -46,7 +52,7 @@ def main():
         if not definitions:
             logging.warning("Definitions file might be empty or missing...")
 
-        run_process(data, definitions, "../config/secrets.ini")
+        run_process(data, definitions, secrets_path)
     except Exception as e:
         logging.critical(f"An unhandled exception occurred: {e}")
     finally:
